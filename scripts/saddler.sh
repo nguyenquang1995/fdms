1069bc3e2784484c5576a2349325c92171c47ecc
ToanDoan <toandoan.coder@gmail.c>
2017-07-03 13:27:09 +0700

    fix ci

--- scripts/saddler.sh
+++ scripts/saddler.sh
@@ -74,7 +74,7 @@ cat app/build/reports/pmd/cpd.xml \
 echo "********************"
 echo "* android lint     *"
 echo "********************"
-cat app/build/outputs/lint-results-debug.xml \
+cat app/build/reports/lint-results.xml \
     | android_lint_translate_checkstyle_format translate \
     | checkstyle_filter-git diff origin/master \
     | saddler report --require saddler/reporter/github --reporter $REPORTER
