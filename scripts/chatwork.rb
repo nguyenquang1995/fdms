4276b2bc9a3a591b8042cccbddc3775335478b94
ToanDoan <toandoan.coder@gmail.c>
2017-07-05 16:08:27 +0700

    fix ci message

--- scripts/chatwork.rb
+++ scripts/chatwork.rb
@@ -2,4 +2,4 @@
 
 	# Create message
 	ChatWork.api_key = "8f247d4a711fb2326012a8ad47243db3"
-	ChatWork::Message.create(room_id: 68078155, body: "[info][To:2110258] [To:1729399] [To:2204808] [To:2357769]  \n\n CI Build Success! \n\n Check em di (yeu) :x! \n Link: #{ARGV[0]} [/info]")
+	ChatWork::Message.create(room_id: 68078155, body: "[info][To:2110258] [To:1729399] [To:2204808] [To:2357769]  \n\n CI Build Success :x!  \n #{ARGV[0]} [/info]")
