690ee7fc4c5cd51bd37b57ba3b7b10edf00f36e6
ongnau <beepig025@gmail.com>
2017-07-10 17:16:49 +0700

    ui

---
+++ app/src/main/java/com/framgia/fdms/screen/exportfile/FileExportContract.java
@@ -0,0 +1,10 @@
+package com.framgia.fdms.screen.exportfile;
+
+/**
+ * Created by beepi on 10/07/2017.
+ * Interface has more function to export file .doc, .xls..
+ */
+public interface FileExportContract {
+    void exportToDocument();
+    void exportToExel();
+}
